6549517c2bf1c0ac08941c83ecd1464f5755ef33
mscoley169 <mscoley169@st.nashcc.edu>
2015-03-05 10:58:11 -0500

    painting class formatter

--- workspace/Painting/src/edu/nashcc/paint/Painting.java
+++ workspace/Painting/src/edu/nashcc/paint/Painting.java
@@ -6,53 +6,62 @@ public class Painting {
 	String title, artist, medium;
 	double price, commission;
 
-	public Painting(String title, String artist, String medium){
+	public Painting(String title, String artist, String medium) {
 		this.title = title;
 		this.artist = artist;
 		this.medium = medium;
-	
+
 	}
-	
-	public Painting(String title, String artist, String medium, double price){
+
+	public Painting(String title, String artist, String medium, double price) {
 		this.title = title;
 		this.artist = artist;
 		this.medium = medium;
 		this.price = price;
-	
+
 	}
-	public void setTitle(String title){
+
+	public void setTitle(String title) {
 		this.title = title;
-		}
-	public void setArtist(String artist){
+	}
+
+	public void setArtist(String artist) {
 		this.artist = artist;
 	}
-	public void setMedium(String medium){
+
+	public void setMedium(String medium) {
 		this.medium = medium;
 	}
-	public void setPrice(double price){
+
+	public void setPrice(double price) {
 		this.price = price;
 	}
-	public String getTitle(){
+
+	public String getTitle() {
 		return this.title;
 	}
-	public String getArtist(){
+
+	public String getArtist() {
 		return this.artist;
 	}
-	public String getMedium(){
+
+	public String getMedium() {
 		return this.medium;
 	}
-	public double getPrice(){
+
+	public double getPrice() {
 		return this.price;
 	}
-	public double getCommission(){
+
+	public double getCommission() {
 		commission = this.price * 0.20;
 		return this.commission;
 	}
-	
-	//Scanner input for setting values
-	public void setUserInput(Painting paint){
+
+	// Scanner input for setting values
+	public void getUserInput(Painting paint) {
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Enter the title: ");
 		this.title = input.nextLine();
 		System.out.println("Enter the artist: ");
@@ -62,6 +71,14 @@ public class Painting {
 		System.out.println("Enter the price: ");
 		this.price = Double.parseDouble(input.nextLine());
 		input.close();
-		}
-	
+	}
+
+	public String formatData() {
+		return String
+				.format("Title: %s \nArtist: %s \nMedium: %s \nPrice: %s \nCommission: %s",
+						this.title, this.artist, this.medium, this.price,
+						this.commission);
+
+	}
+
 }
