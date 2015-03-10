48a0c0e36f924be6dc2a3a39985d52355a14c32d
mscoley169 <mscoley169@st.nashcc.edu>
2015-03-09 21:06:25 -0400

    State/TestState and CertOfDeposit/TestCert
    created two new projects for ch4 assignments, need to fix and finish

---
+++ workspace/State/src/edu/nashcc/state/TestState.java
@@ -0,0 +1,14 @@
+package edu.nashcc.state;
+
+public class TestState {
+
+	public static void main(String[] args) {
+
+		
+		State state1 = new State("North Carolina", 10054192);
+		
+		
+		System.out.println(state1.getStateData());
+	}
+
+}
