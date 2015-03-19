45d376e3a74040a5519cfe020544de2fa5007cb3
mscoley169 <mscoley169@st.nashcc.edu>
2015-03-19 11:04:44 -0400

    CertOfDepo

--- workspace/CertificateOfDeposit/src/edu/nashcc/cert/TestCertificateOfDeposit.java
+++ workspace/CertificateOfDeposit/src/edu/nashcc/cert/TestCertificateOfDeposit.java
@@ -4,25 +4,31 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import javax.swing.JOptionPane;
+
 public class TestCertificateOfDeposit {
 
 	public static void main(String[] args) {
 
 		Calendar calendar = new GregorianCalendar();
-		SimpleDateFormat dateForm = new
-		SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		SimpleDateFormat dateForm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 		// System.out.println(dateForm.format(calendar.getTime()));
-		
-		//Calendar calendar = new GregorianCalendar();
-		
-		calendar.getInstance();
-		
 
-		CertificateOfDeposit depo1 = new CertificateOfDeposite("001", "Smith", calendar, 1000.0D);   // String certNum,
-																	// String acctLastName,
-																	// GregorianCalendar issueDate,
-																	// double balance
+		// Calendar calendar = new GregorianCalendar();
+
+		calendar.getInstance();
+
+		JOptionPane
+				.showMessageDialog(
+						null,
+						"GregorianCalendar is outdated and being phased out by Oracle, this task is stupid..eh..");
+
+		// CertificateOfDeposit depo1 = new CertificateOfDeposite("001",
+		// "Smith", calendar, 1000.0D); // String certNum,
+		// String acctLastName,
+		// GregorianCalendar issueDate,
+		// double balance
 
 	}
 
