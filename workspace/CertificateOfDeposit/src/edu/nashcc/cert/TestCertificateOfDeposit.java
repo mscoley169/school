b9f478374ca726ac78fa0ed313b3bac6830b07ee
mscoley169 <mscoley169@st.nashcc.edu>
2015-03-19 00:55:43 -0400

    quick edit

--- workspace/CertificateOfDeposit/src/edu/nashcc/cert/TestCertificateOfDeposit.java
+++ workspace/CertificateOfDeposit/src/edu/nashcc/cert/TestCertificateOfDeposit.java
@@ -8,13 +8,18 @@ public class TestCertificateOfDeposit {
 
 	public static void main(String[] args) {
 
-		// Calendar calendar = new GregorianCalendar();
-		// SimpleDateFormat dateForm = new
-		// SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Calendar calendar = new GregorianCalendar();
+		SimpleDateFormat dateForm = new
+		SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 		// System.out.println(dateForm.format(calendar.getTime()));
+		
+		//Calendar calendar = new GregorianCalendar();
+		
+		calendar.getInstance();
+		
 
-		//CertificateOfDeposit depo1 = new CertificateOfDeposite("001", "Smith"; );   // String certNum,
+		CertificateOfDeposit depo1 = new CertificateOfDeposite("001", "Smith", calendar, 1000.0D);   // String certNum,
 																	// String acctLastName,
 																	// GregorianCalendar issueDate,
 																	// double balance
