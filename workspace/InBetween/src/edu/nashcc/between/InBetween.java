993fdb8f9a63aa97f4c65496f469a76421215ef3
mscoley169 <mscoley169@st.nashcc.edu>
2015-04-26 17:12:21 -0400

    InBetween.java finished
    TODO: fix ranges printed, prints intLo

--- workspace/InBetween/src/edu/nashcc/between/InBetween.java
+++ workspace/InBetween/src/edu/nashcc/between/InBetween.java
@@ -7,9 +7,45 @@ public class InBetween {
 	private int a, b, c;
 	
 	public static void main(String[] args) {
+		
+		// instantiating InBetween object
 		InBetween x = new InBetween();
 		x.setA();
 		x.setB();
+		
+		// creating StringBuilder object to add results to a list for display
+		StringBuilder sb = new StringBuilder();
+				
+		// determining which is higher and setting to intHi or intLo
+		int intHi, intLo, diff;
+		if (x.getA() > x.getB()){
+			intHi = x.getA();
+			intLo = x.getB();
+		}
+		else if(x.getB() > x.getA()){
+			intHi = x.getB();
+			intLo = x.getA();
+		}
+		else{
+			intHi = 1;
+			intLo = 0;
+		}
+		
+		// determining difference for message
+		diff = intHi - intLo;
+		
+		if(diff <= 1){
+			sb.append("There are no integers between A and B");
+			}
+		else while(intLo < intHi){
+			sb.append(intLo);
+			sb.append("\n");
+			intLo++;
+			}
+		
+		JOptionPane.showMessageDialog(null, sb.toString());
+		
+	
 		
 		
 	}
@@ -28,11 +64,5 @@ public class InBetween {
 	}
 	private int getB(){
 		return this.b;
-	}
-	private void whileCalc(){
-		int x = Math.abs(this.a - this.b);
-		for(x > 0; ){
-			
-		}
 	}
 }
